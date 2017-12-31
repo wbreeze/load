8cafe23a17f3323e6d1a3059d0fb7c148b31e86a
Douglas Lovell <doug@wbreeze.com>
2017-12-31 13:02:41 -0500

    Make the configuration path that enables lib for eager loading

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module Load
   class Application < Rails::Application
     config.autoload_paths += Dir["#{config.root}/lib/"]
+    config.eager_load_paths += Dir["#{config.root}/lib/"]
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
