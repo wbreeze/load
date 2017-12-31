662f672aa9f99200e3f4ae329603b2060b14a997
Douglas Lovell <doug@wbreeze.com>
2017-12-31 11:18:11 -0500

    Test the model

--- test/models/prisoner_test.rb
+++ test/models/prisoner_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PrisonerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Fixture load' do
+    assert(Prisoner.all.count == 2)
+  end
+
+  test 'Capture a prisoner' do
+    serial_number = '717D2124'
+    p = Prisoner.create(
+      name: 'Bob Knievel',
+      rank: 'Stunt Man',
+      serial_number: serial_number)
+    assert(p != nil)
+    assert(Prisoner.all.count == 3)
+    assert(Prisoner.where(serial_number: serial_number).first != nil)
+  end
 end
